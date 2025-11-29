7adf2aac2a17d150c10b5514ed48b42393a3965a
Ultimo15 <kmilonet18@gmail.com>
2025-11-29 11:00:54 -0500

    Actualizar mail.routes.js

--- Backend/routes/mail.routes.js
+++ Backend/routes/mail.routes.js
@@ -4,10 +4,29 @@ const mailController = require("../controllers/mail.controller");
 const { body } = require("express-validator");
 const { authUser, authCaptain } = require("../middlewares/auth.middleware");
 
+// Rutas de verificación de email
 router.get("/verify-user-email", authUser, mailController.sendVerificationEmail);
 router.get("/verify-captain-email", authCaptain, mailController.sendVerificationEmail);
 
-router.post("/:userType/reset-password",  mailController.forgotPassword);
+// Ruta de forgot password
+router.post(
+  "/:userType/forgot-password",
+  [
+    body("email").isEmail().withMessage("Please provide a valid email address")
+  ],
+  mailController.forgotPassword
+);
 
+// Ruta de reset password
+router.post(
+  "/:userType/reset-password",
+  [
+    body("token").notEmpty().withMessage("Token is required"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long")
+  ],
+  mailController.resetPassword
+);
 
 module.exports = router;
